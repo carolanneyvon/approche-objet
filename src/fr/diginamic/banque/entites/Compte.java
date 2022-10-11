dcd4537c7567024648c742d93ce0839fa57b2f63
Carolanne Yvon <elolithia@gmail.com>
2022-10-11 15:23:22 +0200

    tp5

---
+++ src/fr/diginamic/banque/entites/Compte.java
@@ -0,0 +1,14 @@
+package fr.diginamic.banque.entites;
+
+public class Compte {
+
+	private int numeroCompte;
+	private double soldeCompte;
+	
+	public Compte(int numeroCompte, double soldeCompte) {
+		this.numeroCompte = numeroCompte;
+		this.soldeCompte = soldeCompte;
+	}
+	
+	
+}
---
+++ src/fr/diginamic/banque/entites/TestBanque.java
@@ -0,0 +1,12 @@
+package fr.diginamic.banque.entites;
+
+import entites.Facture;
+
+public class TestBanque {
+
+	public static void main(String[] args) {
+		Compte toto = new Compte(123456789, 12500);
+		System.out.println(toto);
+	}
+
+}
