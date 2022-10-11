2f026213889b76ac01e2163cfd3636d6b6b3c2c8
Carolanne Yvon <elolithia@gmail.com>
2022-10-11 16:16:28 +0200

    correction tp 5

--- src/fr/diginamic/banque/entites/TestBanque.java
+++ src/fr/diginamic/banque/entites/TestBanque.java
@@ -4,8 +4,8 @@ package fr.diginamic.banque.entites;
 public class TestBanque {
 
 	public static void main(String[] args) {
-		Compte toto = new Compte(123456789, 12500);
-		System.out.println(toto);
+		Compte compte1 = new Compte(123456789, 12500);
+		System.out.println(compte1);
 	}
 
 }
