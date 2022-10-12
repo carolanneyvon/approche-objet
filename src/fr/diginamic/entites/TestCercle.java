106f26adbcbc1c32497d0e89b89faa18d32dd494
Carolanne Yvon <elolithia@gmail.com>
2022-10-12 17:07:50 +0200

    edit TP CercleFactory

--- src/fr/diginamic/entites/TestCercle.java
+++ src/fr/diginamic/entites/TestCercle.java
@@ -8,7 +8,7 @@ public class TestCercle {
 		Cercle c1 = new Cercle(5);
 		Cercle c2 = new Cercle(8);
 		Cercle c3 = CercleFactory.retourCercle(9);
-		//Cercle[] cercles = {new Cercle(5), new Cercle(8), c};
+		//Cercle[] cercles = {new Cercle(5), new Cercle(8), c3};
 		Cercle[] cercles = {c1, c2, c3};
 		
 		for (int i = 0; i < cercles.length; i++) {
