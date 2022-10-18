b3243da9f883738f6f2b0e42af8271d951ed3176
Carolanne Yvon <elolithia@gmail.com>
2022-10-18 12:13:36 +0200

    TP N°15 (Fichier)- Ex 03 - LireFichierAvecInstanciation

--- src/fichier/LectureFichier.java
+++ src/fichier/LectureFichier.java
@@ -5,21 +5,43 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+
+
 
 public class LectureFichier {
 
 	public static void main(String[] args) throws IOException {
+
+		// Exercice LireFichier
 		String cheminStr = "C:\\Caro\\CDA\\java\\Supports\\7 - Langage Java - Approche Objet\\J5 & J6 - Collections et fichiers\\TP\\recensement.csv";
 		Path path = Paths.get(cheminStr);
 
 		// Lecture et affichage du contenu du fichier
 		List<String> lignes = Files.readAllLines(path, StandardCharsets.UTF_8);
-
 		for (String l : lignes) {
 			System.out.println(l);
 		}
 
+		// Exercice LireFichierAvecInstanciation
+		// ArrayList vide
+				ArrayList<Ville> listeVilles = new ArrayList<>();
+		
+		// Création instance Ville
+		// La méthode split permet de découper une ligne en
+		// tableau de chaine de caractères sur la base d’un séparateur
+		for (int i = 1; i < lignes.size(); i++) {
+			String[] liste = lignes.get(i).split(";");
+			String nom = liste[6];
+			String departement = liste[2];
+			String region = liste[1];
+			int population = Integer.valueOf(liste[9].replaceAll(" ", ""));
+			
+			Ville v = new Ville(nom, departement, region, population);
+			listeVilles.add(v);
+		}
+		System.out.println("Nombre de Ville : "+listeVilles.size());
 	}
 
 }
---
+++ src/fichier/Ville.java
@@ -0,0 +1,57 @@
+package fichier;
+
+public class Ville {
+	private String nom;
+	private String departement;
+	private String region;
+	private int population;
+
+	// constructeur
+	public Ville(String nom, String departement, String region, int population) {
+		super();
+		this.nom = nom;
+		this.departement = departement;
+		this.region = region;
+		this.population = population;
+	}
+
+	@Override
+	public String toString() {
+		return "Ville [nom=" + nom + ", departement=" + departement + ", region=" + region + ", population="
+				+ population + "]";
+	}
+
+	// getters et setters
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getDepartement() {
+		return departement;
+	}
+
+	public void setDepartement(String departement) {
+		this.departement = departement;
+	}
+
+	public String getRegion() {
+		return region;
+	}
+
+	public void setRegion(String region) {
+		this.region = region;
+	}
+
+	public int getPopulation() {
+		return population;
+	}
+
+	public void setPopulation(int population) {
+		this.population = population;
+	}
+
+}
