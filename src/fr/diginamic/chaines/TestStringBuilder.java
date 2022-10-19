afc355d38e6624bc3efa435d59cc9728d7fb7b86
Carolanne Yvon <elolithia@gmail.com>
2022-10-19 11:51:27 +0200

    TP StringBuilder - StringBuilder vs String

--- src/fr/diginamic/chaines/TestStringBuilder.java
+++ src/fr/diginamic/chaines/TestStringBuilder.java
@@ -5,31 +5,29 @@ public class TestStringBuilder {
 	public static void main(String[] args) {
 
 		// avec StringBuilder
-		StringBuilder builder = new StringBuilder();
 		// début chronométrage
 		long debut = System.currentTimeMillis();
-
+		
+		StringBuilder builder = new StringBuilder();
 		for (int i = 1; i <= 100000; i++) {
 			builder.append(i);
 		}
 
 		// fin chronométrage
 		long fin = System.currentTimeMillis();
-		
 		System.out.println("Temps écoulé en millisecondes builder :" + (fin - debut)); // 12ms - 9ms
 
 		// avec String et l’opérateur de concaténation +
-		String string = "";
 		// début chronométrage
 		long debut2 = System.currentTimeMillis();
 
+		String string = "";
 		for (int i = 1; i <= 100000; i++) {
 			string += i;
 		}
 
 		// fin chronométrage
 		long fin2 = System.currentTimeMillis();
-
 		System.out.println("Temps écoulé en millisecondes string :" + (fin2 - debut2)); // 10782ms - 8728ms
 	}
 
